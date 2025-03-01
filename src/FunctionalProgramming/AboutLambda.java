ab19232d93063e9833fab9dfb469638db7d219d9
Prince Kumar <princekumar.80107@gmail.com>
2025-03-01 14:54:00 +0530

    Learning Advance Java

---
+++ src/FunctionalProgramming/AboutLambda.java
@@ -0,0 +1,24 @@
+package FunctionalProgramming;
+
+public class AboutLambda {
+
+    public static void main(String[] args) {
+
+        // no argument
+        // () -> System.out.println("Hello, World");
+
+        // one argument
+        // a -> System.out.println(a); or (a) -> System.out.println(a);
+
+        // multiple argument
+        // (a, b) -> System.out.println(a + b);
+
+        // () -> {
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(a + b);
+        // }
+
+
+    }
+}
---
+++ src/FunctionalProgramming/Basics.java
@@ -0,0 +1,12 @@
+package FunctionalProgramming;
+
+public class Basics {
+
+    // Pure Function
+    public int findSum(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+
+    }
+}
